58dd54bfcd68ac661b3d3e3ecaa4e6401c7bba7d
Hoang Long <tieuyeu0414@gmail.com>
2022-01-17 09:51:56 +0700

    'a'

--- src/controllers/device/route.js
+++ src/controllers/device/route.js
@@ -6,7 +6,7 @@ const deviceAPI = require('./api')
 route.post('/insert', deviceAPI.insertDevice);
 route.put('/edit/:id', deviceAPI.updateDevice);
 route.delete('/delete/:id', deviceAPI.deleteDevice);
-route.get('/', deviceAPI.getFilterDevice);
+route.get('/search', deviceAPI.getFilterDevice);
 route.get('/', deviceAPI.getDataDevice);
 
 module.exports = route;
